8959b2ae15222150feb6c3e8c000ecb6afbd6e13
Toni <ajlozano@users.noreply.github.com>
2023-02-07 11:10:33 +0100

    Initial Commit

---
+++ Pendist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pendist
+//
+//  Created by Toni Lozano Fernández on 7/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
